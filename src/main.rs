f2721bcd100aeda82e5763bd16f204b074c676d0
Eliot996 <math10t6@stud.kea.dk>
2022-04-09 23:06:16 +0200

    writes to file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fmt::format;
+
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = std::env::args().skip(1);
+
+    let key = arguments.next().expect("The key was not there");
+    let value = arguments.next().expect("The value was not there");
+    println!("The key is '{}' and the value is '{}'", key, value);
+
+    let contents = format!("{}\t{}\n", key, value);
+    std::fs::write("kv.db", contents);
 }
