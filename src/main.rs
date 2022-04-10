602946380217159d9cd2845b4c4d1e6aac6039c6
Eliot996 <math10t6@stud.kea.dk>
2022-04-10 07:15:16 +0200

    end of part 1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,49 @@
-use std::fmt::format;
+// https://www.youtube.com/watch?v=lLWchWTUFOQ
+
+use std::collections::HashMap;
+use std::fs;
 
 fn main() {
+    // get args
     let mut arguments = std::env::args().skip(1);
 
+    // get the keys and print the key and value
     let key = arguments.next().expect("The key was not there");
     let value = arguments.next().expect("The value was not there");
     println!("The key is '{}' and the value is '{}'", key, value);
 
+    // concat with a marco, that can take af variable amount of arguments
     let contents = format!("{}\t{}\n", key, value);
-    std::fs::write("kv.db", contents);
+
+    // write to file
+    fs::write("kv.db", contents).unwrap();
+
+
+    let database = Database::new().expect("database::new() crashed");
+}
+
+struct Database {
+    map: HashMap<String, String>
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        // read file
+        let contents = fs::read_to_string("kv.db")?;
+
+        let mut map = HashMap::new();
+        for line in contents.lines() {
+            // parse the string
+            let mut chunks = line.splitn(2, '\t');
+            let key= chunks.next().expect("Key not found");
+            let value= chunks.next().expect("Value not found");
+
+            // populate our map
+            map.insert(key.to_owned(), value.to_owned());
+        }
+
+        Ok(Database {
+            map: map
+        })
+    }
 }
